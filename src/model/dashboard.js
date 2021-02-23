450b84df6e9b9e157606964701b22836d7f2e46e
Firman Azhar Riyadi <33318547+FirmanAzharR@users.noreply.github.com>
2021-02-23 14:18:58 +0700

    fix query dashboard

--- src/model/dashboard.js
+++ src/model/dashboard.js
@@ -5,14 +5,14 @@ module.exports = {
     return new Promise((resolve, reject) => {
       if (filter === 'year') {
         connection.query(
-          'SELECT COUNT(`transaction_id`) AS total_transaksi, YEAR(transaction_created_at) AS tahun FROM TRANSACTION  GROUP BY(YEAR(`transaction_created_at`))',
+          'SELECT COUNT(`transaction_id`) AS total_transaksi, YEAR(transaction_created_at) AS tahun FROM transaction  GROUP BY(YEAR(`transaction_created_at`))',
           (error, result) => {
             !error ? resolve(result) : reject(new Error(error))
           }
         )
       } else {
         connection.query(
-          'SELECT COUNT(`transaction_id`) AS total_transaksi, MONTH(transaction_created_at) AS bulan FROM TRANSACTION WHERE YEAR(`transaction_created_at`) = YEAR(NOW()) GROUP BY(MONTH(`transaction_created_at`))',
+          'SELECT COUNT(`transaction_id`) AS total_transaksi, MONTH(transaction_created_at) AS bulan FROM transaction WHERE YEAR(`transaction_created_at`) = YEAR(NOW()) GROUP BY(MONTH(`transaction_created_at`))',
           (error, result) => {
             !error ? resolve(result) : reject(new Error(error))
           }
@@ -23,7 +23,7 @@ module.exports = {
   getTransactionNow: () => {
     return new Promise((resolve, reject) => {
       connection.query(
-        'SELECT SUM(total) AS total_transaksi,COUNT(transaction_id) AS total_transaction FROM TRANSACTION WHERE  DATE(`transaction_created_at`) = DATE(NOW())',
+        'SELECT SUM(total) AS total_transaksi,COUNT(transaction_id) AS total_transaction FROM transaction WHERE  DATE(`transaction_created_at`) = DATE(NOW())',
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error))
         }
@@ -33,7 +33,7 @@ module.exports = {
   getTransactionYear: () => {
     return new Promise((resolve, reject) => {
       connection.query(
-        'SELECT SUM(total) AS total_transaksi,COUNT(transaction_id) AS total_transaction FROM TRANSACTION WHERE  YEAR(`transaction_created_at`) = YEAR(NOW())',
+        'SELECT SUM(total) AS total_transaksi,COUNT(transaction_id) AS total_transaction FROM transaction WHERE  YEAR(`transaction_created_at`) = YEAR(NOW())',
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error))
         }
@@ -43,7 +43,7 @@ module.exports = {
   totalTransaction: () => {
     return new Promise((resolve, reject) => {
       connection.query(
-        'SELECT COUNT(transaction_id) AS total_transaction FROM TRANSACTION',
+        'SELECT COUNT(transaction_id) AS total_transaction FROM transaction',
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error))
         }
